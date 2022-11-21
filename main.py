636b86eb2bfa99b7f09370a8bf367c14b1331357
Adam <adambox77@gmail.com>
2022-11-21 22:25:54 +0100

    1st commit -project: basic_login_form FastAPI

---
+++ main.py
@@ -0,0 +1,57 @@
+import json
+from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import RedirectResponse
+
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates/")
+
+users_dict = {}
+
+
+# endpointy dla LoginForm
+
+@app.get('/login/')
+async def read_loginForm(request: Request):
+    uname = 'Enter Username'
+    psw = 'Enter password'
+    return templates.TemplateResponse('login_form.html', context={'request': request,
+                                                                  'uname': uname, 'psw': psw})
+
+@app.post('/login/')
+async def loginForm(request: Request, uname: str = Form(...), psw: str = Form(...)):
+    url = 'https://www.youtube.com/watch?v=mM8nGt4Z8lI'
+    with open('logins.json', 'r') as openfile:
+        json_logins = json.load(openfile)
+    for login in json_logins:
+        if login == uname:
+            if json_logins[uname] == psw:
+                return RedirectResponse(url)
+    else:
+        return templates.TemplateResponse('error.html', context={'request': request,
+                                                                 'uname': uname, 'psw': psw})
+#endpointy dla dla signup
+
+@app.get('/signup')
+async def read_signupForm(request: Request):
+    uname = 'Enter Username'
+    psw1 = 'Enter password'
+    psw2 = 'Repeat password'
+    return templates.TemplateResponse('signup_form.html', context={'request': request, 'uname': uname,
+                                                                  'psw1': psw1, 'psw2': psw2})
+
+@app.post('/signup')
+async def signupForm(request: Request, uname: str = Form(...), psw1: str = Form(...), psw2: str = Form(...)):
+    if psw1 == psw2:
+        users_dict[uname] = psw1
+        logins_data = json.dumps(users_dict)
+        with open("logins.json", "w") as outfile:
+            outfile.write(logins_data)
+        return templates.TemplateResponse('signup_form.html', context={'request': request, 'uname': uname,
+                                                                  'psw1': psw1, 'psw2': psw2})
+
+
+
+
+
